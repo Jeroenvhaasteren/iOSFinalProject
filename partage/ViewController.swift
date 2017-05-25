a3bb35b88ea0f590c340263c29c1b24dcc761ab5
Jeroen van Haasteren <jeroenvhaasteren@gmail.com>
2017-05-25 12:40:58 +0200

    Initial Commit

---
+++ partage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  partage
+//
+//  Created by Jeroen van Haasteren on 25/05/2017.
+//  Copyright © 2017 BTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
